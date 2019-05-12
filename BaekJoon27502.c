ce753af2b65ba1aa317a50c0a2630be3da5b6d0e
jihoahn9303 <48744746+jihoahn9303@users.noreply.github.com>
2019-05-13 01:34:31 +0900

    Add files via upload

---
+++ BaekJoon27502.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void quickSort(int* array, int start, int end)
+{
+	if (start >= end)
+		return;
+	
+    int i = start + 1;
+	int j = end;
+	int pivot = start;
+	int temp;
+
+	while (i <= j)
+	{
+		while (array[i] <= array[pivot] && i <= end)
+			i++;
+		while (array[j] >= array[pivot] && j > start)
+			j--;
+		if (i <= j)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		else
+		{
+			temp = array[pivot];
+			array[pivot] = array[j];
+			array[j] = temp;
+		}
+	}
+	
+    quickSort(array, start, j - 1);
+	quickSort(array, j + 1, end);
+}
+
+int main(int argc, char* argv[])
+{
+	int count;
+	scanf("%d", &count);
+
+	int* array = (int*)malloc(sizeof(int) * count);
+	for (int i = 0; i < count; i++)
+	{
+		int input;
+		scanf("%d", &input);
+		array[i] = input;
+	}
+
+	quickSort(array, 0, count - 1);
+	for (int i = 0; i < count; i++)
+		printf("%d\n", array[i]);
+
+	free(array);
+	return 0;
+}
---
+++ BaekJoon2909.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int priceofCandy, numberofZero;
+	scanf("%d %d", &priceofCandy, &numberofZero);
+
+	int tmp = 1;
+	for (int i = 0; i < numberofZero; i++)
+		tmp *= 10;
+	
+	int remainder;
+    remainder = priceofCandy % tmp;
+
+	if (remainder >= tmp / 2)
+		priceofCandy = priceofCandy + (tmp - remainder); 
+	else    priceofCandy -= remainder;
+    
+	printf("%d\n", priceofCandy);
+	return 0;
+}
